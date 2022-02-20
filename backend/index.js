200676c7f126d4558d341285b2cb02b1d9c4f236
fahmi123 <fahimtu456@gmail.com>
2022-02-20 20:32:19 +0700

    crud-mern

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import db from './config/database.js'
+import ProductRouter from './routes/index.js'
+
+const app = express()
+
+try {
+    await db.authenticate();
+    console.log('Database connected...');
+} catch (error) {
+    console.error('Connection error:' , error);
+}
+
+app.use(express.json())
+app.use('/products',ProductRouter)
+
+app.listen(3009, () => {
+    console.log('Server running att 3009');
+})
---
+++ backend/routes/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+import { 
+    getAllProducts,
+    createProduct,
+    getProductById
+ } from '../controllers/Products.js'
+
+const router = express.Router()
+
+router.get('/', getAllProducts)
+router.get('/:id', getProductById)
+router.post('/', createProduct)
+
+export default router
